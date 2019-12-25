cc9886a0679a74461bcf6deb060d1b929911b673
beyondyyh <beyondyyh@126.com>
2019-12-25 14:41:04 +0800

    Add some debug for find-the-duplicate-number

--- algorithms/array/287.findDuplicate.go
+++ algorithms/array/287.findDuplicate.go
@@ -1,16 +1,35 @@
 package main
 
+import "fmt"
+
 func findDuplicate(nums []int) int {
-	// slow, fast := 0, 0
+	fmt.Println(nums)
 	slow, fast := nums[0], nums[nums[0]]
+	fmt.Printf("slow:%d fast:%d\n", slow, fast)
 	for slow != fast {
 		slow, fast = nums[slow], nums[nums[fast]]
+		fmt.Printf("slow:%d fast:%d\n", slow, fast)
 	}
 
-	// 让before，after分别指向链表开始节点，相遇节点
-	before, after := 0, slow
-	for before != after {
-		before, after = nums[before], nums[after]
+	fmt.Println("------------------")
+	// 让slow，fast分别指向链表开始节点，相遇节点
+	slow = 0 // fast此时就是指向相遇节点
+	fmt.Printf("slow:%d fast:%d\n", slow, fast)
+	for slow != fast {
+		slow, fast = nums[slow], nums[fast]
+		fmt.Printf("slow:%d fast:%d\n", slow, fast)
 	}
-	return before
+	fmt.Println("**************")
+	return slow
 }
+
+// [1 3 4 2 2]
+// slow:1 fast:3
+// slow:3 fast:4
+// slow:2 fast:4
+// slow:4 fast:4
+// ------------------
+// slow:0 fast:4
+// slow:1 fast:2
+// slow:3 fast:4
+// slow:2 fast:2
