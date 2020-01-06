0a37958a2c4de327cd92771a7eb12197090c41c0
beyondyyh <beyondyyh@126.com>
2020-01-06 18:40:19 +0800

    Add simplify-path

--- algorithms/string/71.simplifyPath.go
+++ algorithms/string/71.simplifyPath.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
 	"gopl.io/interview2020/Leetcode/algorithms/mystack"
 )
 
-// run: go test -v 71.*
+// 1. 用栈
+// 2. 先把字符串以"/"为分隔符分割成数组，此数组包含"路径字符串"、""、"."、".."这四种情况;
+// 3. 遍历数组, 当s[i]==".."并且栈不空时pop, 当s[i]!="" && s[i]!="." && s[i]!=".."时，把s[i]路径压入栈;
+// 4. 如果栈空, 返回"/", 栈非空, 从栈底部（注意不是栈顶）开始拼接字符串
 func simplifyPath(path string) string {
 	stack := mystack.NewStack()
 	parts := strings.Split(path, "/")
@@ -30,4 +34,9 @@ func simplifyPath(path string) string {
 		items = append(items, (stack.Seek(i)).(string))
 	}
 	return "/" + strings.Join(items, "/")
+}
+
+// golang标准库提供了牛逼的此方法
+func cleanFilepath(path string) string {
+	return filepath.Clean(path)
 }
--- algorithms/string/71.simplifyPath_test.go
+++ algorithms/string/71.simplifyPath_test.go
@@ -4,12 +4,15 @@ import (
 	"testing"
 )
 
-func Test_simplifyPath(t *testing.T) {
-	cases := []struct {
-		name     string
-		input    string
-		expected string
-	}{
+type entry71 struct {
+	name     string
+	input    string
+	expected string
+}
+
+// build_simplifyPath_case return test cases
+func build_simplifyPath_case() []entry71 {
+	cases := []entry71{
 		{
 			name:     "x1",
 			input:    "/../",
@@ -36,11 +39,25 @@ func Test_simplifyPath(t *testing.T) {
 			expected: "/a/b/c",
 		},
 	}
+	return cases
+}
 
-	for _, tt := range cases {
+// run: go test -v 71.*
+func Test_simplifyPath(t *testing.T) {
+	for _, tt := range build_simplifyPath_case() {
 		t.Run(tt.name, func(t *testing.T) {
 			if output := simplifyPath(tt.input); output != tt.expected {
 				t.Errorf("simplifyPath(%s)=%s, expected=%s", tt.input, output, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_cleanFilepath(t *testing.T) {
+	for _, tt := range build_simplifyPath_case() {
+		t.Run(tt.name, func(t *testing.T) {
+			if output := cleanFilepath(tt.input); output != tt.expected {
+				t.Errorf("cleanFilepath(%s)=%s, expected=%s", tt.input, output, tt.expected)
 			}
 		})
 	}
