ba43a8939143fe1b63980f7b8b320c5c58461465
beyondyyh <beyondyyh@126.com>
2020-01-10 15:19:47 +0800

    Modify variable name

--- algorithms/myqueue/641.MyCircularDeque.go
+++ algorithms/myqueue/641.MyCircularDeque.go
@@ -2,18 +2,18 @@ package myqueue
 
 // MyCircularDeque structure declare
 type MyCircularDeque struct {
-	front  int   // 指向队列头部第1个有效数据的位置
-	rear   int   // 指向队列尾部（即最后1个有效数据）的下一个位置，即下一个从队尾入队元素的位置
-	length int   // 队列长度，非容量，slice动态库容
-	nums   []int // 队列元素
+	front int   // 指向队列头部第1个有效数据的位置
+	rear  int   // 指向队列尾部（即最后1个有效数据）的下一个位置，即下一个从队尾入队元素的位置
+	cap   int   // 队列容量
+	nums  []int // 队列元素
 }
 
 // NewMyCircularDeque Initialize structure. Set the size of the deque to be k.
 func NewMyCircularDeque(k int) *MyCircularDeque {
-	length := k + 1
+	capacity := k + 1
 	return &MyCircularDeque{
-		length: length,
-		nums:   make([]int, length),
+		cap:  capacity,
+		nums: make([]int, capacity, capacity), // 申请一个len 和 cap都为k+1的数组
 	}
 }
 
@@ -23,9 +23,9 @@ func (q *MyCircularDeque) InsertFront(x int) bool {
 		return false
 	}
 
-	// front指针前移一位，取模是关键，指针前移需要先加q.length再整体取模，因为可能出现负数
-	// 而后移是加法不需要加q.length
-	q.front = (q.front - 1 + q.length) % q.length
+	// front指针前移一位，取模是关键，指针前移需要先加q.cap再整体取模，因为可能出现负数
+	// 而后移是加法不需要加q.cap
+	q.front = (q.front - 1 + q.cap) % q.cap
 	q.nums[q.front] = x
 	return true
 }
@@ -39,7 +39,7 @@ func (q *MyCircularDeque) InsertLast(x int) bool {
 	// 先赋值再移动指针，因为rear存的是最后1个有效数据的下一个位置
 	q.nums[q.rear] = x
 	// rear指针后移一位，取模是关键
-	q.rear = (q.rear + 1) % q.length
+	q.rear = (q.rear + 1) % q.cap
 	return true
 }
 
@@ -50,7 +50,7 @@ func (q *MyCircularDeque) DeleteFront() bool {
 	}
 
 	// front指针后移一位
-	q.front = (q.front + 1) % q.length
+	q.front = (q.front + 1) % q.cap
 	return true
 }
 
@@ -61,7 +61,7 @@ func (q *MyCircularDeque) DeleteLast() bool {
 	}
 
 	// rear指针前移一位
-	q.rear = (q.rear - 1 + q.length) % q.length
+	q.rear = (q.rear - 1 + q.cap) % q.cap
 	return true
 }
 
@@ -80,7 +80,7 @@ func (q *MyCircularDeque) GetRear() int {
 		return -1
 	}
 
-	return q.nums[(q.rear-1+q.length)%q.length]
+	return q.nums[(q.rear-1+q.cap)%q.cap]
 }
 
 // IsEmpty Checks whether the circular deque is empty or not.
@@ -90,5 +90,5 @@ func (q *MyCircularDeque) IsEmpty() bool {
 
 // IsFull Checks whether the circular deque is full or not.
 func (q *MyCircularDeque) IsFull() bool {
-	return (q.rear+1)%q.length == q.front
+	return (q.rear+1)%q.cap == q.front
 }
